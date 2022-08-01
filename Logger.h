d90dd0b577e8ba5284d9804fd164cfa2b32ec77b
ANKITA SETH <97723029+Ankita-seth@users.noreply.github.com>
2022-08-01 23:00:24 +0530

    Update Logger.h

--- Logger.h
+++ Logger.h
@@ -2,16 +2,13 @@
 #include<iostream>
 #include <fstream>
 
-
-
 using namespace std;
 namespace LoggerClass
 {
-
-	class Logger
+	class Logger                                                                                      //Abstract class 
 	{
 	public:
-		virtual ostream& GetStream() = 0;
+		virtual ostream& GetStream() = 0;                                                         //Pure Virtual Function
 	};
 
 	class ConsoleLogger :public Logger
@@ -22,20 +19,18 @@ namespace LoggerClass
 	class FileLogger :public Logger
 	{
 	private:
-		ofstream m_filelogger;
-
+		ofstream m_filelogger;                                                                    //File create for write
 		public:
 			//FILE* pFile = fopen("ankita.log", "a");
 			//std::ostream os;
-
-			FileLogger(std::string filename)
+			FileLogger(std::string filename)                                                  //Constructor
 			{
-				m_filelogger.open(filename.c_str(), ios_base::out);
+				m_filelogger.open(filename.c_str(), ios_base::out);                       //File opened
 			};
 
-			~FileLogger()
+			~FileLogger()                                                                     //Destructor
 			{
-				m_filelogger.close();
+				m_filelogger.close();                                                     //File closed
 			};
 
    		    ostream& GetStream();
@@ -48,7 +43,7 @@ namespace LoggerClass
 		static Logger* CreateConsoleLogger();
 	};
 
-	static LoggerClass::Logger* g_logger = LoggerClass::LoggerFactory::CreateFileLogger("ankita.log");
+	static LoggerClass::Logger* g_logger = LoggerClass::LoggerFactory::CreateFileLogger("ankita.log");                 //Object Creation of logger class
 	//static Logger* g_logger = LoggerFactory::CreateConsoleLogger();
-    #define LOG() g_logger->GetStream()
+    #define LOG() g_logger->GetStream()                                                                     //Macro defined
 }
